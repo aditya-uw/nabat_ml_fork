c34b16c8e76f20c29be3dc967532479188b1d1a0
Aditya Krishna <adkris1002@gmail.com>
2024-07-24 10:28:49 -0700

    added nabat_ml v2release with trained model

--- db.py
+++ db.py
@@ -13,9 +13,9 @@ from shutil import copyfile
 
 class NABat_DB:
 
-    def __init__(self, p='db0',):
+    def __init__(self, path='db0', class_list=None):
 
-        self.path = p
+        self.path = path
         try:
             os.mkdir(self.path)
         except:
@@ -30,10 +30,18 @@ class NABat_DB:
         self.conn = self._create_connection(self.db_name)
         self.conn.row_factory = namedtuple_factory
         self.cursor = self.conn.cursor()
-        self.cursor.execute("PRAGMA journal_mode=WAL")
 
         self.fastCon = self._create_connection(self.db_name)
         self.fastCursor = self.fastCon.cursor()
+
+        self.cursor.execute("PRAGMA journal_mode=WAL")
+        self.fastCursor.execute("PRAGMA journal_mode=WAL")
+
+        self._set_species_list(class_list)
+
+    def __del__(self):
+        self.conn.close()
+        self.fastCon.close()
 
     def _create_connection(self, db_file):
         conn = None
@@ -46,6 +54,11 @@ class NABat_DB:
 
         return conn
 
+    def species_from_species_code(self, species_code):
+        for s in self.query('select * from species;'):
+            if s.species_code == species_code:
+                return s    
+                    
     def to_csv(self, table):
         names = self.query('PRAGMA table_info({});'.format(table))
         data = self.query('select * from {};'.format(table))
@@ -54,7 +67,43 @@ class NABat_DB:
             writer.writerow([n[1] for n in names])
             writer.writerows(data)
 
-    def get_files(self, file_id=None, file_name=None):
+    def delete(self):
+        self.conn.close()
+        os.remove(self.db_name)
+        self.__init__(self.path, self.class_list)
+
+    def set_species_list(self, grts_id):
+        self.insert(
+            "UPDATE species set available = 0 where id not in (select species_id from species_grts where grts_id = ?);", (grts_id,))
+        return self.query('select * from species;')
+
+    def _set_species_list(self, class_list):
+        if class_list is not None:
+            self.insert(
+                "UPDATE species set available = 0 where species_code not in ({})".format(','.join(['?'] * len(class_list))), class_list)
+
+    def get_full_sl(self, grts_id):
+        rows = self.query(
+            'select s.id, sg.species_id from species s left join species_grts sg on sg.species_id = s.id and grts_id = ? order by id', (grts_id,))
+        return np.array([0 if s.species_id == None else 1 for s in rows])
+
+    def set_location(self, grts_id, grts_cell_id, sample_frame, location, sublocation):
+        self.insert("DELETE FROM location;")
+        self.insert("INSERT INTO location (grts_id, grts_cell_id, sample_frame, location, sublocation) VALUES (?,?,?,?,?);",
+                    (grts_id, grts_cell_id, sample_frame, location, sublocation))
+
+    def get_location(self):
+        return self.query("select * from location;")
+
+    def get_file(self, file=None):
+        if file is not None:
+            return self.query(
+                "SELECT * FROM file where name = ?;", (file,))
+        else:
+            return self.fastQuery(
+                "SELECT name FROM file;")
+
+    def get_training_files(self, file_id=None, file_name=None):
         if file_id is not None:
             return self.query(
                 "SELECT id FROM file where id = ?;", (file_id,))
@@ -64,6 +113,28 @@ class NABat_DB:
         else:
             return self.fastQuery(
                 "SELECT name FROM file;")
+        
+    def get_classification_files(self):
+        return self.fastQuery(
+            """ 
+                with x as (
+                select file_id,  pulse_id, max(confidence) as con, count(*)  as c
+                from pulse p 
+                join prediction pp on pp.pulse_id = p.id
+                join species s on s.id = pp.species_id
+                where s.available = 1
+                group by 1,2
+                )
+                select f.id, f.name, f.duration,  f.sample_rate,  avg(x.con)
+                from x
+                join file f on f.id = x.file_id
+                group by 1
+                order by sum(case when x.con > 0.7 then x.con else 0 end) desc
+                --by max(x.con) + avg(x.con) + (CASE 
+                 --   WHEN x.c >=10 THEN 1 
+                 --   ELSE (x.c/10)
+                 --   END) desc;
+            """)
 
     def get_pulses(self, file_id=None):
         if file_id is not None:
@@ -73,7 +144,17 @@ class NABat_DB:
             return self.fastQuery(
                 "SELECT p.*, draw FROM pulse p join file f on f.id = p.file_id;")
 
-    def add_file(self, name, duration, sample_rate, manual_id, grts_id, draw=None):
+    def get_predictions(self, pulse_id, file_id):
+
+        if pulse_id:
+            return self.query(
+                "SELECT pulse_id, species_code, confidence FROM prediction p join species s on p.species_id = s.id where pulse_id = ? and available = 1 order by species_code;", (pulse_id,))
+        elif file_id:
+            return self.query(
+                "SELECT pulse_id, species_code, confidence FROM pulse pp join prediction p on p.pulse_id = pp.id join species s on p.species_id = s.id where file_id = ? and available = 1 order by pulse_id, species_code;", (file_id,))
+
+
+    def add_training_file(self, name, duration, sample_rate, manual_id, grts_id, draw=None):
 
         if draw == None:
             r = random.random()
@@ -88,15 +169,85 @@ class NABat_DB:
             "INSERT INTO file (draw, name, duration, sample_rate, manual_id, grts_id) VALUES (?,?,?,?,?,?);", (draw, name, duration, sample_rate, manual_id, grts_id))
         return file_id, draw
 
-    def add_pulse(self, file_id, frequency, amplitude, sig_noise, offset, time,  window, path):
+    def add_classification_file(self, name, duration, sample_rate, grts_id):
+        return self.insert(
+            "INSERT INTO file (name, duration, sample_rate, grts_id) VALUES (?,?,?,?);", (name, duration, sample_rate, grts_id))
+    
+
+    def add_training_pulse(self, file_id, frequency, amplitude, sig_noise, offset, time,  window, path):
         return self.insert(
             "INSERT INTO pulse (file_id, frequency, amplitude, sig_noise, offset, time, window, path) VALUES (?,?,?,?,?,?,?,?);", (file_id, frequency, amplitude, sig_noise, offset, time, window, path))
 
-    def add_predictions(self, data):
+    def add_classification_pulse(self, file_id, frequency, amplitude, sig_noise, offset, time,  window):
+        return self.insert(
+            "INSERT INTO pulse (file_id, frequency, amplitude, sig_noise, offset, time, window) VALUES (?,?,?,?,?,?,?);", (file_id, frequency, amplitude, sig_noise, offset, time, window))
+    
+    def add_training_predictions(self, data):
         self.conn.executemany(
             "insert into prediction (model_name, pulse_id, confidence, species_id) values (?,?,?,?);", data)
         self.conn.execute('commit;')
 
+    def add_classification_predictions(self, values):
+        self.conn.executemany(
+            'INSERT INTO prediction (pulse_id, species_id, confidence)  values (?,?,?);', values)
+        self.conn.execute('commit;')
+
+    def get_predictions_file(self, file_name):
+        return self.query("""
+        
+            with samples as (
+                select
+                    f.id as file_id,
+                    count(*) pc
+                from 
+                    file f
+                join
+                    pulse p on f.id = p.file_id
+                where
+                    f.name = ?
+                group by 1
+            ) 
+            , conf as (
+                select
+                    p.file_id,
+                    pp.species_id,
+                    count(p.id) num_samples,
+                    sum(confidence)/(pc) sc
+                from 
+                    samples
+                join 
+                    pulse p on p.file_id = samples.file_id
+                join
+                    prediction pp on pp.pulse_id = p.id
+                group by
+                    p.file_id, pp.species_id
+            )
+            , result  as (
+                select
+                    conf.file_id,
+                    name,
+                    f.grts_id,
+                    s.species_code,
+                    s.id as species_id,
+                    max(conf.sc) conf
+                from 
+                    conf
+                join
+                    file f on f.id = conf.file_id
+                join 
+                    species s on s.id = conf.species_id
+                group by 
+                    1,2 ,3
+			)
+			select
+                r.*
+            from
+                result r
+            join
+                species_grts sg on sg.grts_id = r.grts_id and sg.species_id = r.species_id
+        ;
+        """, (file_name,))
+    
     def query(self, query, args={}):
         try:
             self.cursor.execute(query, args)
